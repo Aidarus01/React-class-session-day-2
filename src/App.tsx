8b9fa58187216c43c13c45d935121e4e3d1f1401
Aidarus Farah <aidarus.farah@gmail.com>
2020-04-08 19:45:39 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Heading } from './components/Heading';
+import Person from './components/Person';
+import logo from './logo.svg';
+import './App.css';
+
+const myPersonFunction = (input: string):string=> {
+  return 'Our person is important' + input;
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Heading/>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Person name ="Billy-Joe Bob" callbackMethod ={myPersonFunction}/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Heading.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+ export class Heading extends React.Component {
+    render () 
+    {
+        return <h1> Our First Component</h1>
+    }
+}
+
